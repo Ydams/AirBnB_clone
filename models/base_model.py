5a1919e6ac0dc41915d1b20d1a541ee4fca6a6f3
Ydams <Damola4542@gmail.com>
2024-03-11 03:52:20 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -4,21 +4,25 @@
 """
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """
         """
+    
         if kwargs:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at()
+            for key,value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self,key,value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
-        """
-
-        """
-       self.updated_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
@@ -33,5 +37,5 @@ class BaseModel:
     def __str__(self):
         """
         """
-        class_name = self.__class__.name__
-        return f'[{self.__class__.name__}] ({self.id}] {self.__dict__}'
+        class_name = self.__class__.__name__
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
