b49e687c204da0ea5b1c8a2e2dd608677e660dd2
Ydams <Damola4542@gmail.com>
2024-03-12 05:12:11 -0700

    update

--- tests/test_model/test_base_model.py
+++ tests/test_model/test_base_model.py
@@ -3,46 +3,74 @@
 
 """
 import unittest
+import os
+from datetime import datetime
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
-class TestBasemodel(unittest.TestCase):
-    def test_init(self):
-        my_model = BaseModel()
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.base_instance = BaseModel()
 
-        self.assertIsNotNone(my_model.id)
-        self.assertIsNotNone(my_model.created_at)
-        self.assertIsNotNone(my_model.updated_at)
+    def tearDown(self):
+        del self.base_instance
 
-    def test_save(self):
-        my_model = BaseModel()
+    def test_instance_attributes(self):
+        self.assertTrue(hasattr(self.base_instance, 'id'))
+        self.assertTrue(hasattr(self.base_instance, 'created_at'))
+        self.assertTrue(hasattr(self.base_instance, 'updated_at'))
 
-        initial_updated_at = my_model.updated_at
+    def test_id_type(self):
+        self.assertIsInstance(self.base_instance.id, str)
 
-        current_updated_at = my_model.save()
+    def test_created_at_type(self):
+        self.assertIsInstance(self.base_instance.created_at, datetime)
 
-        self.assertNotEqual(initial_update_at, current_updated_at)
+    def test_updated_at_type(self):
+        self.assertIsInstance(self.base_instance.updated_at, datetime)
 
-    def test_to_dict(self):
-        my_model = BaseModel()
+    def test_save_updates_updated_at(self):
+        old_updated_at = self.base_instance.updated_at
+        self.base_instance.save()
+        new_updated_at = self.base_instance.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
 
-        my_model_dict = My_model.to_dict()
 
-        self.assertIsInstance(my_model_dict, dict)
+class TestFileStorage(unittest.TestCase):
+    def setUp(self):
+        self.storage = FileStorage()
+        self.base_instance = BaseModel()
+        self.test_json_file = "test_file.json"
 
-        self.assertEqual(my_model_dict["__class__"], 'BaseModel')
-        self.assertEqual(my_model_dict['id'], my_model.id)
-        self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
-        self.assertEqual(my_model_dict["updated_at"], my_model.created_at.isoformat())
+    def tearDown(self):
+        del self.storage
+        del self.base_instance
+        if os.path.exists(self.test_json_file):
+            os.remove(self.test_json_file)
 
-    def test_str(self):
-        my_model = BaseModel()
+    def test_new_method(self):
+        self.assertIn("BaseModel." + self.base_instance.id, self.storage.all())
 
-        self.assertTrue(str(my_model).startwith('[BaseModel]'))
+    def test_save_method(self):
+        self.base_instance.save()
+        with open(self.test_json_file, 'r') as file:
+            data = file.read()
+            self.assertTrue(data)
+            self.assertIn("BaseModel." + self.base_instance.id, data)
 
-        self.assertIn(my_model.id, str(my_model))
+    def test_reload_method(self):
+        self.base_instance.save()
+        self.storage.reload()
+        self.assertIn("BaseModel." + self.base_instance.id, self.storage.all())
 
-        self.assertIn(str(my_model.__dict__), str(my_model))
+    def test_all_method(self):
+        self.base_instance.save()
+        all_objs = self.storage.all()
+        self.assertIsInstance(all_objs, dict)
+        self.assertIn("BaseModel." + self.base_instance.id, all_objs)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     unittest.main()
+
